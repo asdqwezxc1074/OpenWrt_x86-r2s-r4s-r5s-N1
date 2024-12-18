32c394a0b9cac4ac5d805068b206f64fcf4ac78b
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-12-18 12:23:42 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -8,6 +8,6 @@ rm -rf package/firmware package/boot/uboot-envtools package/kernel target/linux/
 
 git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/firmware package/boot/uboot-envtools package/kernel target/linux/qualcommax
 
-#wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/target/linux/qualcommax/ipq60xx/target.mk -P target/linux/qualcommax/ipq60xx/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/target/linux/qualcommax/ipq60xx/target.mk -P target/linux/qualcommax/ipq60xx/
 
-#rm -rf target/linux/qualcommax/patches-6.6/06*-qca-*.patch
+rm -rf target/linux/qualcommax/patches-6.6/06*-qca-*.patch
